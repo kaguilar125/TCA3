d4fc710a2666d5a80db0b4f4c26107f6f0fb5fbd
kaguilar125 <113131954+kaguilar125@users.noreply.github.com>
2024-04-09 20:04:04 -0400

    push 4

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-function imgclick() {
+  function imgclick() {
     alert("Fun Fact! Bees fly about 20 mph.");
+  }
+  function img1click() {
+    alert("Fun Fact! Bees have 5 eyes.");
+  }
+  function img2click() {
+    alert("Fun Fact! An average beehive can hold around 50,000 bees.");
+  }
+  function img3click() {
+    alert("Fun Fact! Male bees in the hive are called drones.");
+  }
+  function img4click() {
+    alert("Fun Fact! Number of eggs laid by queen: 2,000 per day is the high.");
   }
